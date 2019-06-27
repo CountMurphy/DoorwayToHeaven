67b13eb79b60f423ff414be716c57469720df0ff
cgugas <spartan.1086@gmail.com>
2019-06-26 20:18:31 -0500

    added low power mode

--- software/main.c
+++ software/main.c
@@ -3,12 +3,41 @@
 #include "pins.h"
 #include "ring.h"
 
+
+/* Configuration for enter VLPR mode. Core clock = 4MHz. */
+static const clock_manager_user_config_t g_defaultClockConfigVlpr = {   
+    .mcgliteConfig =
+    {    
+        .mcglite_mode       = kMcgliteModeLirc8M,   // Work in LIRC_8M mode.
+        .irclkEnable        = true,  // MCGIRCLK enable.
+        .irclkEnableInStop  = false, // MCGIRCLK disable in STOP mode.
+        .ircs               = kMcgliteLircSel8M, // Select LIRC_8M.
+        .fcrdiv             = kMcgliteLircDivBy1,    // FCRDIV is 0.
+        .lircDiv2           = kMcgliteLircDivBy1,    // LIRC_DIV2 is 0.
+        .hircEnableInNotHircMode         = false, // HIRC disable.
+    },
+    .simConfig =
+    {    
+        .er32kSrc  = kClockEr32kSrcOsc0,   // ERCLK32K selection, use OSC.
+        .outdiv1   = 1U, // divide by 2, core clock = 8 MHz / 2
+        .outdiv4   = 3U, // divide by 4, bus clock = 4 MHz / 4
+    },
+    .oscerConfig =
+    {   
+        .enable       = false, // OSCERCLK disable.
+        .enableInStop = false, // OSCERCLK disable in STOP mode.
+    }
+};
+
+
 int main()
 {
     hardware_init();
     OSA_Init();
     ChevronInit();
 
+
+    CLOCK_SYS_SetConfiguration(&g_defaultClockConfigVlpr);
     //main function taken care of in interrupt
     GPIO_DRV_WritePinOutput(Reset.pinName,0);
     GPIO_DRV_WritePinOutput(Reset.pinName,1);
